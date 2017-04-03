21bd115651131be3fe05d94033b8cccf0c14ae08
John Barbone <jbarbone@Johns-MacBook-Pro.local>
2017-04-03 11:38:42 -0400

    committed

--- UC_CoreData_Test/GraphView.swift
+++ UC_CoreData_Test/GraphView.swift
@@ -13,10 +13,11 @@ class GraphView: UIView {
     var vals : [Int] = [ 0, 80, 40, 50, 60, 55, 55, 67, 85]
     var number : Int = 9
     var maxHeight: Int = 85
-    
+    var newDates: [String] = []
     var dates: [String] = [] // these dates are in "short-style" format and are in the same order as the values of the actual data in the values array
     
     override func draw(_ rect: CGRect){
+         //let attrs = [NSFontAttributeName: UIFont(name: "HelveticaNeue-Thin", size: 36)!, NSParagraphStyleAttributeName: NSParagraphStyle()]
         let width = Int(bounds.width)
         let height = Int(bounds.height)
         let p1 = UIBezierPath()
@@ -26,10 +27,17 @@ class GraphView: UIView {
             w += 1
             p1.addLine(to: CGPoint(x: w * width/(number + 1), y: p))
             p1.move(to: CGPoint(x: w * width/(number + 1), y: p))
+            //var num = w - 1
+            //let value = newDates[num]
+            //value.draw(at: CGPoint(x: w * width/(number + 1), y: height) , attributes: attrs, context: nil)
+            
         }
         
         p1.lineWidth = 5
         p1.stroke()
+        
+        
+        //string.draw(with: CGRect(x: 32, y: 32, width: 448, height: 448), options: .usesLineFragmentOrigin, attributes: attrs, context: nil)
         
         
     }
@@ -38,7 +46,7 @@ class GraphView: UIView {
         
         var modvals : [Int] = []
         for n in 1...number {
-            
+            newDates.insert(dates[dates.count - n], at: 0)
             modvals.insert(vals[vals.count - n], at: 0)
         }
         var finalvals : [Int] = []
